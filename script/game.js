e5c4e2980d71733401cc67eed32ef771b39e069a
awsms <x.a.z.x.e.e@protonmail.com>
2021-12-29 10:30:20 +0100

    changing stuff

--- script/game.js
+++ script/game.js
@@ -1,14 +1,62 @@
-var duck = document.getElementById('duck');
-var duckScore = document.getElementById('duckScore');
-var hunterScore = document.getElementById('hunterScore');
-var resetButton = document.getElementById('reset');
-var timer = document.getElementById('timer');
+const duck = document.getElementById('duck');
+const duckScore = document.getElementById('duckScore');
+const hunterScore = document.getElementById('hunterScore');
+const resetButton = document.getElementById('reset');
+const timer = document.getElementById('timer');
+const intervalDuck = 10
 
+var gameOver = false
 duckScore.textContent = 'turbo claqué'
+// miroir le jpg quand presse gauche / droite
 
-duckXLocation = 0
-duckYLocation = 0
 
+// duck's location on x & y axes
+duckXLocation = 625
+duckYLocation = 425
+var duckX = duck.style.transform = 'translateX('+duckXLocation+'px)'
+var duckY = duck.style.transform = 'translateY('+duckYLocation+'px)'
+
+
+// onclick events part
+
+//document.getElementById('new_game').addEventListener('click', () => {
+//  newGame();
+//  });
+
+document.getElementById('duck').addEventListener('mousedown', () => {
+   hunterHit();
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+   reset();
+});
+
+var currentKeyPressed = {
+   up: false,
+   down: false,
+   left: false,
+   right: false
+} 
+
+var duckLookingRight = true
+
+// if (currentKeyPressed.left) {
+//    duckLookingRight = false
+// }
+
+// if (!duckLookingRight) {
+//    duck.style.transform = 'scaleX(-1)'
+// }
+
+
+
+
+function resetKeyPressed() {
+   currentKeyPressed.up = false,
+   currentKeyPressed.down = false,
+   currentKeyPressed.left = false,
+   currentKeyPressed.right = false
+}
 
 // modify duck's location on x axis
 // function duckX(distance) {
@@ -35,35 +83,107 @@ duckYLocation = 0
 function duckPosition(x,y) {
    duckXLocation += x
    duckYLocation += y
-   if (duckXLocation > 1250) {duckXLocation = 1250}
+   if (duckXLocation > 1350) {duckXLocation = 1350}
    if (duckXLocation < 0) {duckXLocation = 0}
-   if (duckYLocation > 850) {duckYLocation = 850}
+   if (duckYLocation > 825) {duckYLocation = 825}
    if (duckYLocation < 0) {duckYLocation = 0}
    duck.style.transform = 'translate('+duckXLocation+'px,'+duckYLocation+'px)'
 }
 
 
 
-// input handling part
-window.addEventListener("keydown", function(event) {
-   if (event.code === "ArrowDown"){
+
+ 
+
+
+//input handling part
+// list of events happening when the key is hold down
+window.addEventListener("keydown", function(e) {
+   if (e.code === "ArrowDown"){
       // move the duck 50px down when arrow down key is pressed
       // duckY(50);
-      duckPosition(0,16);
-   } else if (event.code === "ArrowUp"){
-      duckPosition(0,-16)
+      // duckPosition(0,16);
+      // when the down arrow is pressed, change the currentKeyPressed property for down key to true
+      currentKeyPressed.down = true
+   } if (e.code === "ArrowUp"){
+      // duckPosition(0,-16)
+      currentKeyPressed.up = true
       // duckY(-50);
-   } else if (event.code === "ArrowLeft"){
-      duckPosition(-16,0)
+   } if (e.code === "ArrowLeft"){
+      // duckPosition(-16,0)
+      currentKeyPressed.left = true
       // duckX(-50);
-   } else if (event.code === "ArrowRight"){
-      duckPosition(16,0)
+   } if (e.code === "ArrowRight"){
+      // duckPosition(16,0)
+      currentKeyPressed.right = true
       // duckX(50);
+   // } resetKeyPressed()
+}});
+
+//list of events happening when the key is released
+window.addEventListener("keyup", function(e) {
+   if (e.code === "ArrowDown"){
+      // move the duck 50px down when arrow down key is pressed
+      // duckY(50);
+      // duckPosition(0,16);
+      currentKeyPressed.down = false
+   } if (e.code === "ArrowUp"){
+      // duckPosition(0,-16)
+      currentKeyPressed.up = false
+      // duckY(-50);
+   } if (e.code === "ArrowLeft"){
+      // duckPosition(-16,0)
+      currentKeyPressed.left = false
+      // duckX(-50);
+   } if (e.code === "ArrowRight"){
+      // duckPosition(16,0)
+      currentKeyPressed.right = false
+      // duckX(50);
+   // } resetKeyPressed()
+}});
+
+
+
+// setInterval(() => {
+//    (currentKeyPressed.up
+//       ? duckPosition(0,-16)
+//       : currentKeyPressed.down
+//          ? duckPosition(0,16)
+//          : duckPosition(0,0))
+ 
+//    currentKeyPressed.left
+//       ? duckPosition(-16,0)
+//       : currentKeyPressed.right
+//          ? duckPosition(16,0)
+//          : duckPosition(0,0)
+// }, intervalDuck);
+
+
+setInterval(() => {
+   if (currentKeyPressed.up) {
+      duckPosition(0,-9)
+   } else if (currentKeyPressed.down) {
+      duckPosition(0,9)
    }
-});
 
 
+   if (currentKeyPressed.right) {
+      duckPosition(9,0)
+   } else if (currentKeyPressed.left) {
+      duckPosition(-9,0)
+      duck.style.transform = 'scaleX(-1)'
+   }
+}, intervalDuck);
 
+function reset() {
+   time = 120
+   hunterScoreCount = 0
+   hunterScore.textContent = 0
+   duckScore = 0
+   duckScore.textContent = 0
+   duckXLocation = 625
+   duckYLocation = 425
+}
   
 
 // update score for the duck player every 10sec
@@ -95,23 +215,16 @@ function whoWins(duckScoreCount,hunterScoreCount) {
 
 
 var time = 120
-var interval = setInterval(function(){
+var timerInterval = setInterval(function(){
    time -= 1
    timer.textContent = time+'s';
 
    if (time == 0) {
-      clearInterval(interval)
+      clearInterval(timerInterval)
       whoWins(duckScoreCount,hunterScoreCount)
+      gameOver = True
    }
 } ,1000);
-
-function reset() {
-   time = 120
-   hunterScoreCount = 0
-   hunterScore.textContent = 0
-   duckScore = 0
-   duckScore.textContent = 0
-}
 
 /* scoring part */
 // hunterScore.textContent = 0
@@ -120,3 +233,5 @@ function hunterHit() {
    // hunterScoreCount += 1
    hunterScore.textContent = (hunterScoreCount += 1)
 }
+
+currentKeyPressed.left ? duck.style.transform = 'scaleX(-1)' : duck.style.transform = 'scaleX(1)'
